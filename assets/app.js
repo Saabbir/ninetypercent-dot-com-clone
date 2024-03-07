96e3f59207524a4216cc0760722510abb0dfa4e9
Saabbir Hossain <thesaabbir@gmail.com>
2024-03-07 21:56:47 +0600

    07032024

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,45 @@
-document.body.addEventListener("click", handleBodyClicks);
+void (function () {
+  document.body.addEventListener("click", handleBodyClicks);
 
-function handleBodyClicks(clickEvent) {
-  if (clickEvent.target.closest(".c-header__hamburger")) {
-    const nav = document.querySelector(".c-header__nav");
-    if (nav) {
-      nav.classList.toggle("is-visible");
+  function handleBodyClicks(clickEvent) {
+    if (clickEvent.target.closest(".c-header__hamburger")) {
+      const nav = document.querySelector(".c-header__nav");
+      if (nav) {
+        nav.classList.toggle("is-visible");
+      }
+      clickEvent.target
+        .closest(".c-header__hamburger")
+        .classList.toggle("is-open");
     }
-    clickEvent.target
-      .closest(".c-header__hamburger")
-      .classList.toggle("is-open");
   }
-}
+
+  // Handle product page functionality
+  if (location.pathname.includes("/products")) {
+    const swiperEl = document.querySelector(".swiper");
+
+    if (swiperEl) {
+      void (function pollForSwiper() {
+        if (window.Swiper) {
+          const swiper = new Swiper(".swiper", {
+            // Optional parameters
+            direction: "horizontal",
+            loop: false,
+
+            // If we need pagination
+            pagination: {
+              el: ".swiper-pagination",
+            },
+
+            // Navigation arrows
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
+        } else {
+          setTimeout(pollForSwiper, 100);
+        }
+      })();
+    }
+  }
+})();
